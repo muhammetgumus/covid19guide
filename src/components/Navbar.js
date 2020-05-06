27a95d05d9b582dc561b3ef197faf8be4f0ed5bf
Muhammet Gumus <muhammetgms95@gmail.com>
2020-05-07 02:34:13 +0300

    searchbar functionalities improved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,16 +5,76 @@ import '../App.css';
 
 
 class Navbar extends React.Component {
-    handleSearch() {
-        let searchbar = document.getElementById('searchbar')
-        let listElement = document.createElement('ul');
-        let listItem = document.createElement('li');
-        listItem.textContent="DENEME1"
-        listItem.value="DENEME2"
-        listItem.title="DENEME3"
-        searchbar.appendChild(listElement);
-        listElement.appendChild(listItem)
+   
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchbarArr: []
+        };
+        this.handleSearch = this.handleSearch.bind(this);
+    }
 
+    handleSearch(event) {
+        let totalResult=0;
+        let searchbar = document.getElementById('searchbar')
+        let container = document.createElement('div');
+
+        if(document.getElementById("unorderedList")!=null){
+            document.getElementById("unorderedList").remove()
+        }
+        let listElement = document.createElement('ul');
+        listElement.id="unorderedList"
+        listElement.innerHTML=''
+
+      /*  for (const x of listElement.childNodes) {
+            listElement.remove(x);
+        }*/
+     //   while (container.firstChild) {
+           // container.removeChild(container.lastChild);
+            container.innerHTML='';
+       //   }
+
+       // while (listElement.firstChild) {
+          
+       
+            
+            //   }
+      //  if
+        if (this.state.searchbarArr.length >= 1) {
+            for (let x of this.state.searchbarArr) {
+                if (x.includes(event.target.value)) {
+                    
+                    let listItem = document.createElement('li');
+                    listItem.id = Math.ceil(Math.random() * 100)
+                    listItem.textContent = x;
+                    listItem.value = x;
+                    listItem.title = x;
+                    
+                    
+                    listElement.appendChild(listItem);
+                    container.appendChild(listElement)
+                    searchbar.after(container);
+                    totalResult++
+                
+                    
+            
+                }
+            }
+
+        }
+      /*  let listItem = document.createElement('li');
+        listItem.id = Math.ceil(Math.random() * 100)
+        
+        listItem.textContent = "DENEME1"
+        listItem.value = "DENEME2"
+        listItem.title = "DENEME3"
+
+       
+        listElement.appendChild(listItem)
+        container.appendChild(listElement)
+        searchbar.after(container);
+ */
+        //document.getElementById('searchbar')
 
     }
     render() {
@@ -36,21 +96,36 @@ class Navbar extends React.Component {
             console.log(countriesData);
             const isArrived = setInterval(() => {
                 if (countriesData !== undefined) {
-/*
-                    //console.log("IN NAVBARRRRR " + countries)
-                    let selection = document.getElementById("countries")
+                    
+                    let tempArr=[]
                     countriesData.forEach(country => {
 
-                        let option = document.createElement('option')
+                        tempArr.push(country.label);
+
+                        /*let option = document.createElement('option')
                         option.text = country.label
                         option.name = country.value
-                        selection.appendChild(option);
+                        selection.appendChild(option);*/
+
                     })
-                    console.log("AFTERR")
-                    console.log(selection.childNodes);
-                   
-*/
-clearInterval(isArrived)
+                        this.setState({
+                            searchbarArr: [...tempArr]
+                        })
+                    /*
+                                        //console.log("IN NAVBARRRRR " + countries)
+                                        let selection = document.getElementById("countries")
+                                        countriesData.forEach(country => {
+                    
+                                            let option = document.createElement('option')
+                                            option.text = country.label
+                                            option.name = country.value
+                                            selection.appendChild(option);
+                                        })
+                                        console.log("AFTERR")
+                                        console.log(selection.childNodes);
+                                       
+                    */
+                    clearInterval(isArrived)
                 }
             }, 20000)
 
