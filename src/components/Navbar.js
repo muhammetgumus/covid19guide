c3c065b232fdadb11508daabc49852a1572dca40
Muhammet Gumus <muhammetgms95@gmail.com>
2020-05-07 23:30:23 +0300

    searchbar functionalites fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,20 +26,8 @@ class Navbar extends React.Component {
         listElement.id="unorderedList"
         listElement.innerHTML=''
 
-      /*  for (const x of listElement.childNodes) {
-            listElement.remove(x);
-        }*/
-     //   while (container.firstChild) {
-           // container.removeChild(container.lastChild);
             container.innerHTML='';
-       //   }
-
-       // while (listElement.firstChild) {
-          
        
-            
-            //   }
-      //  if
         if (this.state.searchbarArr.length >= 1) {
             for (let x of this.state.searchbarArr) {
                 if (x.includes(event.target.value)) {
@@ -62,19 +50,9 @@ class Navbar extends React.Component {
             }
 
         }
-      /*  let listItem = document.createElement('li');
-        listItem.id = Math.ceil(Math.random() * 100)
-        
-        listItem.textContent = "DENEME1"
-        listItem.value = "DENEME2"
-        listItem.title = "DENEME3"
-
-       
-        listElement.appendChild(listItem)
-        container.appendChild(listElement)
-        searchbar.after(container);
- */
-        //document.getElementById('searchbar')
+      if(event.target.value=="" || event.target.value==null){
+          container.innerHTML=''
+      }
 
     }
     render() {
@@ -111,45 +89,17 @@ class Navbar extends React.Component {
                         this.setState({
                             searchbarArr: [...tempArr]
                         })
-                    /*
-                                        //console.log("IN NAVBARRRRR " + countries)
-                                        let selection = document.getElementById("countries")
-                                        countriesData.forEach(country => {
                     
-                                            let option = document.createElement('option')
-                                            option.text = country.label
-                                            option.name = country.value
-                                            selection.appendChild(option);
-                                        })
-                                        console.log("AFTERR")
-                                        console.log(selection.childNodes);
-                                       
-                    */
                     clearInterval(isArrived)
                 }
             }, 20000)
 
-
-
-
         })
-
-
-
-
 
     }
 
     componentDidMount() {
         this.getCountriesFunc();
-
-
     }
-
 }
-
-
-
-
-
 export default Navbar;
