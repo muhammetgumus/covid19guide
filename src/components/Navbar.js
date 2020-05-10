2941ffec4c42e192864543ca28bd7fe5d8f55aa2
Muhammet Gumus <muhammetgms95@gmail.com>
2020-05-11 01:45:30 +0300

    clock added to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,70 +1,76 @@
 import React from 'react';
 import { getSummaryData } from '../service/MainService.js'
 import { Select } from 'semantic-ui-react'
+import { getCurrentDate } from '../utils/Utility.js'
 import '../App.css';
 
 
 class Navbar extends React.Component {
-   
+
     constructor(props) {
         super(props);
         this.state = {
-            searchbarArr: []
+            searchbarArr: [],
+            currentDate: ''
         };
         this.handleSearch = this.handleSearch.bind(this);
-       // this.handleBodyClick =this.handleBodyClick.bind(this);
+        // this.handleBodyClick =this.handleBodyClick.bind(this);
     }
-/*handleBodyClick(event){
-    document.getElementsByName("body").addEventListener('click',()=>{document.getElementById('container').innerHTML=''})
-}*/
+    /*handleBodyClick(event){
+        document.getElementsByName("body").addEventListener('click',()=>{document.getElementById('container').innerHTML=''})
+    }*/
     handleSearch(event) {
-        let totalResult=0;
+        let totalResult = 0;
         let searchbar = document.getElementById('searchbar')
         let container = document.createElement('div');
-        container.id="searchResultsContainer"
-        container.style.position="relative"
-        container.style.zIndex="100"
+        container.id = "searchResultsContainer"
+        container.style.position = "relative"
+        container.style.zIndex = "100"
 
-        if(document.getElementById("searchResults")!=null){
+        if (document.getElementById("searchResults") != null) {
             document.getElementById("searchResults").remove()
         }
         let tableElement = document.createElement('table');
-        tableElement.id="searchResults"
-        tableElement.innerHTML=''
+        tableElement.id = "searchResults"
+        tableElement.innerHTML = ''
 
-            container.innerHTML='';
-       
+        container.innerHTML = '';
+
         if (this.state.searchbarArr.length >= 1) {
-            
+
             for (let x of this.state.searchbarArr) {
                 if (x.toLowerCase().includes(event.target.value.toLowerCase())) {
-                    if(totalResult!=5){
-                    let tableRow = document.createElement('tr');
-                    let tableData = document.createElement('td');
-                    tableData.id = Math.ceil(Math.random() * 100)
-                    tableData.textContent = x;
-                    tableData.value = x;
-                    tableData.title = x;
-                    //tableData.style={backrogundColor:"red"}
+                    if (totalResult != 5) {
+                        let tableRow = document.createElement('tr');
+                        let tableData = document.createElement('td');
+                        tableData.id = Math.ceil(Math.random() * 100)
+                        tableData.textContent = x;
+                        tableData.value = x;
+                        tableData.title = x;
+                        //tableData.style={backrogundColor:"red"}
 
-                    tableRow.appendChild(tableData)
-                    tableElement.appendChild(tableRow);
-                    container.appendChild(tableElement)
-                    searchbar.after(container);
-                    totalResult++
-                }
+                        tableRow.appendChild(tableData)
+                        tableElement.appendChild(tableRow);
+                        container.appendChild(tableElement)
+                        searchbar.after(container);
+                        totalResult++
+                    }
                 }
             }
 
         }
-      if(event.target.value=="" || event.target.value==null){
-          container.innerHTML=''
-      }
+        if (event.target.value == "" || event.target.value == null) {
+            container.innerHTML = ''
+        }
 
     }
     render() {
+        let curr = this.state.currentDate;
         return (
             <div className="card-navbar">
+                <div className="clock">{
+                    curr
+                }</div>
                 <h5 className="hashtag">#evdekal #stayhome</h5>
                 <input type="text" className="searchbar" placeholder="Search the country" id="searchbar" onChange={this.handleSearch}></input>
             </div>
@@ -81,15 +87,15 @@ class Navbar extends React.Component {
             console.log(countriesData);
             const isArrived = setInterval(() => {
                 if (countriesData !== undefined) {
-                    
-                    let tempArr=[]
+
+                    let tempArr = []
                     countriesData.forEach(country => {
                         tempArr.push(country.label);
                     })
-                        this.setState({
-                            searchbarArr: [...tempArr]
-                        })
-                    
+                    this.setState({
+                        searchbarArr: [...tempArr]
+                    })
+
                     clearInterval(isArrived)
                 }
             }, 500)
@@ -100,6 +106,12 @@ class Navbar extends React.Component {
 
     componentDidMount() {
         this.getCountriesFunc();
-       }
+        this.updateDate = setInterval(() => {
+            this.setState({ currentDate: getCurrentDate() })
+        }, 1000)
+    }
+    componentWillUnmount() {
+        clearInterval(this.updateDate);
+    }
 }
 export default Navbar;
---
+++ src/utils/Utility.js
@@ -0,0 +1,14 @@
+
+export function getCurrentDate() {
+    let date = new Date();
+    let day = date.getDate();
+    let month = String(date.getMonth()+1).length ==1 ?`0${date.getMonth()}` : date.getMonth();
+    let year = date.getFullYear();
+    let seconds = date.getSeconds().toString().length ==1 ?`0${date.getSeconds()}` : date.getSeconds();
+    let minutes = date.getMinutes().toString().length ==1 ?`0${date.getMinutes()}` : date.getMinutes();
+    let hours = date.getHours().toString().length ==1 ?`0${date.getHours()}` : date.getHours();
+
+   // console.log(`${day}-${month}-${year} ${hours}:${minutes}:${seconds}`)
+
+    return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+}
