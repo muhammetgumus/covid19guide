00563abc65ea8a7b1b0cac6cc5ebaa9fe09011bd
Muhammet Gumus <muhammetgms95@gmail.com>
2020-05-21 23:28:56 +0300

    internalization constants added

---
+++ src/static/Internalization.js
@@ -0,0 +1,34 @@
+
+export const en = {
+    NewConfirmed:"Günlük Vakalar",
+    TotalConfirmed:"Total Confirmed",
+    NewDeaths:"New Deaths",
+    TotalDeaths:"Total Deaths",
+    NewRecovered:"New Recovered",
+    Date:"Date",
+    Country:"Country",
+    CountryCode:"Country Code",
+    Latitude:"Latitude",
+    Longitude:"Longitude",
+    Cases:"Cases",
+    stayHome:"#stayHome",
+    placeHolder:"Search Country"
+
+}
+
+
+export const tr= {
+    NewConfirmed:"Günlük Vakalar",
+    TotalConfirmed:"Toplam Vaka",
+    NewDeaths:"Günlük Ölüm",
+    TotalDeaths:"Toplam Ölüm",
+    NewRecovered:"Günlük İyileşenler",
+    Date:"Tarih",
+    Country:"Ülke",
+    CountryCode:"Ülke Kodu",
+    Latitude:"Enlem",
+    Longitude:"Boylam",
+    Cases:"Vaka Sayısı",
+    stayHome:"#evdekal",
+    placeHolder:"Ülke Ara"
+}
